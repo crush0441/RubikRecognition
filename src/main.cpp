8998616cbf7c35fedd26d91d95b3cdd8985cce5d
wuyucong <362883665@qq.com>
2017-12-13 15:23:22 +0800

    bwmethod ok～

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 using namespace cv;
 using namespace std;
 
-char * imgname = "../img1.jpg";
+char * imgname = "../img2.jpg";
 
 void bgr2BW(Mat bgrimg,Mat * BW_res);
 void img_preview(Mat img);
@@ -21,12 +21,16 @@ int main(int argc, char** argv)
     imshow("raw",raw_img);
     bgr2BW(raw_img,&bw_img);
 
+
+    findContours();
+
+    imshow("res",bw_img);
     //cout<<raw_img<<endl;
     waitKey(0);
  	return 0; 
 }
 
-void bgr2BW(Mat bgrimg,Mat * BW_res)
+void bgr2BW(Mat bgrimg,Mat *BW_res)
 {
     Mat channel[3];  
     Mat E(bgrimg.size(),CV_8UC1,Scalar(255));
@@ -34,28 +38,58 @@ void bgr2BW(Mat bgrimg,Mat * BW_res)
     Mat tmp;
     img_preview(BW);
     img_preview(E);
+    Mat ele = getStructuringElement(MORPH_RECT,Size(3,3));//get kernel
 
     split(bgrimg,channel); 
-    for(int i=2;i<3;i++)
+    for(int i=0;i<3;i++)
     {
     tmp=channel[i].clone();
 
-    img_preview(tmp);
+    //img_preview(tmp);
 
     threshold(tmp, tmp, 25, 255.0, CV_THRESH_BINARY);
-    
-    img_preview(tmp);
-    Mat ele = getStructuringElement(MORPH_RECT,Size(3,3));//get kernel
+
+    //img_preview(tmp);
+
     erode(tmp,tmp,ele);
 
-    bitwise_or(tmp,BW,tmp);
+    bitwise_or(tmp,BW,BW);
 
-    img_preview(tmp);
+    // img_preview(BW);
+
+    tmp=channel[i].clone();
 
 
+    Mat grad_x,grad_y,abs_grad_x,abs_grad_y;
+    //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
+    //Scharr( tmp, grad_x, CV_8UC1, 1, 0);
+    GaussianBlur( tmp, tmp, Size(5,5), 0, 0, BORDER_DEFAULT );
 
+    Sobel( tmp, grad_x, CV_8UC1, 2, 0, 3);
+    convertScaleAbs( grad_x, abs_grad_x );
 
-    }
+    /// Gradient Y
+    //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
+    //Scharr( tmp, grad_y, CV_8UC1, 0, 1);
+    Sobel( tmp, grad_y, CV_8UC1, 0, 2, 3);
+    convertScaleAbs( grad_y, abs_grad_y );
+
+    /// Total Gradient (approximate)
+    addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, tmp);
+    // img_preview(tmp);
+    threshold(tmp, tmp, 25, 255.0, CV_THRESH_BINARY);
+    
+    bitwise_not(tmp,tmp);
+    erode(tmp,tmp,ele);
+
+    bitwise_and(E,tmp,E);
+    // img_preview(tmp);
+}
+    bitwise_and(E,BW,BW);
+    erode(BW,BW,ele);
+
+    *BW_res=BW.clone();
+
 }
 
 
